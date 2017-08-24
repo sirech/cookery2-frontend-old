6fd83a050cda5ee8e7e702b904cfc990efd00036
Mario Fernandez <mario.fernandez@xing.com>
2017-08-24 23:11:23 +0200

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux'
 import configureStore from './configure-store'
 import registerServiceWorker from './registerServiceWorker'
 
+import 'bootstrap/dist/css/bootstrap.css'
+
 const store = configureStore()
 
 ReactDOM.render(
